d68a8d39f026f5d99e2b4b327ef1152e3c41c116
Curious Paul <170408025@live.unilag.edu.ng>
2020-07-30 01:42:13 +0100

    added config

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from .extensions import db,migrate
 from .cpanel import cpanel
 from .main import main
+from config import config
 
 # App Factory
-def __call__():
+def __call__(config_name):
     app = Flask (__name__)
 
+    # Add config from config.py
+    app.config.from_object(config[çonfig_name])
+
     db.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app,db)
 
     app.register_blueprint(cpanel,url_prefix='/cpanel') # our admin panel, not the one for NGOs
     app.register_blueprint(main) # all site-buidling related requests go here
--- config.py
+++ config.py
@@ -1,1 +1,32 @@
+import os
 
+basedir = os.getcwd()
+
+class Config:
+    """
+        Base config class, contains config that are generic to all other 
+        sub-config classes for various purposes
+    """
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    SQALCHEMY_DATABASE_URI = f"sqlite:///{basedir}/ohw13Dev.sqlite"
+    FLASK_ENV=Development
+    DEBUG=True
+
+class TestingConfig(Config):
+    SQALCHEMY_DATABASE_URI = f"sqlite:///{basedir}/ohw13Test.sqlite"
+    FLASK_ENV=Testing
+    DEBUG=True
+
+class ProductionConfig(Config):
+    SQALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') # Heroku Demands this
+    FLASK_ENV=Production
+    DEBUG=True
+
+config = {
+    "default":DevelopmentConfig
+    "development":DevelopmentConfig,
+    "testing":TestingConfig,
+    "production":ProductionConfig
+}
