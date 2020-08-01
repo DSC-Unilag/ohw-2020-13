5509279585f44e17470cece51e4477d58a96e62e
lordghostx <solomonghost9@gmail.com>
2020-08-01 02:10:16 +0100

    backend reset

--- backend/app/cpanel/views.py
+++
@@ -1,7 +0,0 @@
-from . import cpanel
-from flask import render_template,request,make_response,url_for,redirect
-
-@cpanel.route('/',methods=['GET'])
-@cpanel.route('/index',methods=['GET'])
-def index():
-    return render_template("cpanel-index.html"),200
--- backend/manage.py
+++
@@ -1,8 +0,0 @@
-from app import __call__,db,migrate
-import os
-
-app = __call__('default' or os.getenv('FLASK_CONFIG'))
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(app=app,db=db)
