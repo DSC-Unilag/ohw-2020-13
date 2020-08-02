65b937139195a90f7b96484a919bf9bdb2b85524
Curious Paul <170408025@live.unilag.edu.ng>
2020-08-02 09:59:07 +0100

    updated views

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from config import config
-from .main import main
-from .admin import admin
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
+
+    bcrypt.init_app(app)
+    db.init_app(app)
 
     # config app
     app.config.from_object(config[str(config_name)])
 
     # register Blueprints
+    from .main import main
+    from .admin import admin
+    
     app.register_blueprint(main)
     app.register_blueprint(admin,url_prefix='/admin')
 
--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
+from datetime import timedelta
 basedir = os.getcwd()
 
 class Config:
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PERMANENT_SESSION_LIFETIME= timedelta(minutes=30)
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir}/devDb.db"
