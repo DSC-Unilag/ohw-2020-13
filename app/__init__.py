14b49204b15e0618a246aae0df9c593df7d15d1b
Curious Paul <170408025@live.unilag.edu.ng>
2020-08-02 10:39:13 +0100

    added stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,20 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from .extensions import db,bcrypt
 from config import config
 
-bcrypt = Bcrypt()
-db = SQLAlchemy()
-
-def create_app(config_name):
+def __call__(config_name):
     app = Flask(__name__)
 
     bcrypt.init_app(app)
     db.init_app(app)
 
     # config app
-    app.config.from_object(config[str(config_name)])
+    app.config.from_object(config[config_name])
 
     # register Blueprints
     from .main import main
     from .admin import admin
-    
+
     app.register_blueprint(main)
     app.register_blueprint(admin,url_prefix='/admin')
 
---
+++ app/extensions.py
@@ -0,0 +1,6 @@
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
