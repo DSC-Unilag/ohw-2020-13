c74d990a568dfcf0fca491c3eb902d60fcd54274
lordghostx <solomonghost9@gmail.com>
2020-08-15 21:23:01 +0100

    fixed auth

--- app/main/views.py
+++ app/main/views.py
@@ -1,35 +1,39 @@
 from . import main
-from flask import render_template,redirect,flash,url_for,request,make_response,session
+from uuid import uuid4
+from flask import render_template, redirect, flash, url_for, request, make_response, session
 from flask import current_app as app
-from app.extensions import db,bcrypt
+from app.extensions import db, bcrypt
 from models import Admin
 from sqlalchemy.exc import IntegrityError
 from functools import wraps
 import datetime as d
 
 
-@main.route('/',methods=['GET'])
+@main.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@main.route('/signup',methods=['GET','POST'])
+
+@main.route('/signup/', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         password = bcrypt.generate_password_hash(request.form['password'])
-        new_admin = Admin(first_name=request.form['firstName'],last_name=request.form['lastName'],email=request.form['email'],password=password)
+        new_admin = Admin(
+            first_name=request.form['firstName'], last_name=request.form['lastName'], email=request.form['email'], password=password, admin_id=str(uuid4()))
         try:
-            db.session.flush(objects=[new_admin])
+            db.session.add(new_admin)
             db.session.commit()
-            flash(u'Welcome, Signup Complete - Login please','success')
-            return redirect(url_for('main.login')),200
+            flash(u'Welcome, Signup Complete - Login please', 'success')
+            return redirect(url_for('main.login')), 200
         except IntegrityError:
             db.session.rollback()
             error = 'User Already Exists'
-            return render_template('signup.html',msg=error),400
+            return render_template('signup.html', msg=error), 400
     elif request.method == 'GET':
-        return render_template('signup.html'),200
+        return render_template('signup.html'), 200
 
-@main.route('/login',methods=['GET','POST'])
+
+@main.route('/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
@@ -40,40 +44,39 @@ def login():
         # find user with matching email
         user = Admin.query.filter_by(email=email).first()
         if not user:
-            flash(u"No such user found",'error')
-            return render_template('login.html'),404
+            flash(u"No such user found", 'error')
+            return render_template('login.html'), 404
         else:
-            if bcrypt.check_password_hash(user.password,password):
+            if bcrypt.check_password_hash(user.password, password):
                 session.permanent = True
                 session['admin_id'] = user.admin_id
-                return redirect(url_for('main.dashboard')),200
-            flash(u'Incorrect Password','error')
-            return render_template('login.html'),400
+                return redirect(url_for('main.dashboard')), 200
+            flash(u'Incorrect Password', 'error')
+            return render_template('login.html'), 400
 
 
 # Authorization decorator
 def login_required(f):
     @wraps(f)
-    def function(*args,**kwargs):
-        admin_id = None
-        if admin_id in session:
+    def function(*args, **kwargs):
+        if 'admin_id' in session:
             admin_id = session['admin_id']
             # find admin with matching ID
             user = Admin.query.filter_by(admin_id=admin_id).first()
             if user:
-                return f(user,*args,**kwargs)
+                return f(user, *args, **kwargs)
             else:
-                flash(u'Invalid Login credentials','error')
-                return redirect(url_for('main.login')),401
+                flash(u'Invalid Login credentials', 'error')
+                return redirect(url_for('main.login')), 401
         else:
-            flash(u'You\'re not logged in','error')
-            return redirect(url_for('main.login')),401
-        
+            flash(u'You\'re not logged in', 'error')
+            return redirect(url_for('main.login')), 401
+
     return function
 
-@main.route('/dashboard',methods=['POST','GET'])
+
+@main.route('/dashboard', methods=['POST', 'GET'])
 @login_required
-def dashboard():
+def dashboard(user):
     if request.method == 'GET':
-        return render_template('dashboard.html'),200
-    
+        return render_template('dashboard.html'), 200
